301b0b2d3bda0de5859a44f51fbb45cabd05610a
zukaishi <ishizuka215@gmail.com>
2021-03-31 07:34:38 +0900

    update

---
+++ 1791/1791.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Go!!!")
+	s := [][]int{{1, 2}, {2, 3}, {4, 2}}
+	//fmt.Println(s)
+	r := findCenter(s)
+	fmt.Println(r)
+}
+
+func findCenter(edges [][]int) int {
+	var ｓ []int
+	for _, v := range edges {
+		for _, v2 := range v {
+			// 途中
+		}
+	}
+	return 1
+}
---
+++ 1791/README.md
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/find-center-of-star-graph/
+
+```go
+
+```
