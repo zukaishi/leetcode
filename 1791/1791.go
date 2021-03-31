0c7ca9bee9ae979325276c3ac36174af1b93eb9b
zukaishi <ishizuka215@gmail.com>
2021-04-01 07:12:42 +0900

    update

--- 1791/1791.go
+++ 1791/1791.go
@@ -13,11 +13,23 @@ func main() {
 }
 
 func findCenter(edges [][]int) int {
-	var ｓ []int
+	n1 := 0
+	n2 := 0
+	ans := 0
 	for _, v := range edges {
-		for _, v2 := range v {
-			// 途中
+		if n1 == v[0] || n2 == v[0] {
+			ans = v[0]
+			break
+		} else if n1 == v[1] || n2 == v[1] {
+			ans = v[1]
+			break
+		}
+		if n1 == 0 {
+			n1 = v[0]
+		}
+		if n2 == 0 {
+			n2 = v[1]
 		}
 	}
-	return 1
+	return ans
 }
