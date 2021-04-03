e0dbf6b41f668a172c05dcd01fb5e12a4a6ab2a8
zukaishi <ishizuka215@gmail.com>
2021-04-03 10:41:32 +0900

    update

--- test/test.py
+++ test/test.py
@@ -1,1 +1,21 @@
 print("Hello world!")
+
+class Solution:
+    def findCenter(self, edges) -> int:
+        list = []
+        ans = 0
+        for v in edges:
+            if v[0] in list:
+                ans = v[0]
+                break
+            elif v[1] in list:
+                ans = v[1]
+            list.insert(0,v[0])
+            list.insert(0,v[1])
+        return ans
+
+int_list = [[1,2],[2,3],[4,2]]
+
+s = Solution()
+test = s.findCenter(int_list)
+print(test)
