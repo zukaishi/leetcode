af9345015989f14d6fc62307a95593fdfc7a36ab
zukaishi <ishizuka215@gmail.com>
2021-04-05 07:27:25 +0900

    update

--- 1672/1672.py
+++ 1672/1672.py
@@ -2,11 +2,14 @@ print("maximumWealth")
 
 class Solution:
     def maximumWealth(self, accounts) -> int:
-        return 1
-        
+        max = 0
+        for v in accounts:
+            s = sum(v)
+            if s > max:
+                max = s
+        return max
 
 int_list = [[1,2,3],[3,2,1]]
-
 s = Solution()
 test = s.maximumWealth(int_list)
 print(test)
