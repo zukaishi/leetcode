8e805ae1d8a4e854bed6c2b1da48bc01ed261cae
zukaishi <ishizuka215@gmail.com>
2021-04-04 10:42:12 +0900

    update

--- 1346/1346.py
+++ 1346/1346.py
@@ -2,10 +2,21 @@ print("Hello world!")
 
 class Solution:
     def checkIfExist(self, arr) -> bool:
-        return True
+        check = False
+        v_count = 0
+        lista = []
+        for v in arr:
+            lista.append([v_count,v])
+            v_count += 1
+        print(lista)
 
+        for i,v in lista:
+            for i2,v2 in lista:
+                if v == v2 * 2 and i != i2:
+                    check = True
+        return check
 
-list = [10,2,5,3]
+list = [-2,0,10,-19,4,6,-8]
 s = Solution()
 test = s.checkIfExist(list)
 print(test)
