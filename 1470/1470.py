e6de0e291c0e4389d6afa2b370de374b1e93aee5
zukaishi <ishizuka215@gmail.com>
2021-04-06 06:35:09 +0900

    update

---
+++ 1470/1470.py
@@ -0,0 +1,13 @@
+print("shuffle")
+
+class Solution:
+    def shuffle(self, nums, n) -> List[int]:
+
+        return_s = [1,2,3]
+        return return_s
+
+nums = [2,5,1,3,4,7]
+n =3
+s = Solution()
+test = s.shuffle(nums,nums)
+print(test)
