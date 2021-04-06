cb84b1128a4fa961668eb3b3d81602ca227a254e
zukaishi <ishizuka215@gmail.com>
2021-04-06 16:12:07 +0900

    update

--- 1470/1470.py
+++ 1470/1470.py
@@ -1,16 +1,16 @@
 print("shuffle")
 
 class Solution:
-    def shuffle(self, nums, s) -> [int]:
-        n = 0
+    def shuffle(self, nums, n) -> [int]:
+        i = 0
+        rlist = []
         for _ in nums:
-            print(nums[n:n+s:1])
-            n += s
-            if n >= len(nums):
+            rlist = rlist + nums[i:i+1:1]
+            rlist = rlist + nums[n+i:n+i+1:1]
+            i += 1
+            if i >= n:
                 break
-
-        return_s = [1,2,3]
-        return return_s
+        return rlist
 
 nums = [2,5,1,3,4,7]
 n =3
