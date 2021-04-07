43bb8da64ce9a75cbd7571b847e1a5091daac2ca
zukaishi <ishizuka215@gmail.com>
2021-04-08 08:41:01 +0900

    update

---
+++ 1512/1512.py
@@ -0,0 +1,10 @@
+print("numIdenticalPairs")
+
+class Solution:
+    def numIdenticalPairs(self, nums) -> int:
+        return 0
+
+int_list = [1,2,3,1,1,3]
+s = Solution()
+test = s.numIdenticalPairs(int_list)
+print(test)
