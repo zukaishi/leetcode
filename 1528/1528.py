6cb68b90469dc2976d95525b16bcbb3669da889f
zukaishi <ishizuka215@gmail.com>
2021-04-09 08:19:15 +0900

    update

---
+++ 1528/1528.py
@@ -0,0 +1,11 @@
+print("restoreString")
+
+class Solution:
+    def restoreString(self, s, indices) -> str:
+        return ""
+
+
+s = "codeleet"
+indices = [4,5,6,7,0,2,1,3]
+s = Solution()
+print(s.restoreString(s,indices))
