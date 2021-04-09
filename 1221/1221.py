54d9dd0d122e07364d383298fe88ec3b72613a7f
zukaishi <ishizuka215@gmail.com>
2021-04-10 06:46:54 +0900

    update

---
+++ 1221/1221.py
@@ -0,0 +1,9 @@
+print("balancedStringSplit")
+
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        return 0
+
+s ="RLRRLLRLRL"
+solution = Solution()
+print(solution.balancedStringSplit(s))
