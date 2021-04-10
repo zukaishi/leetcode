47930eade9a80b64d168c0a740b4217b0d3a24fc
zukaishi <ishizuka215@gmail.com>
2021-04-10 14:24:52 +0900

    update

--- 1221/1221.py
+++ 1221/1221.py
@@ -2,10 +2,15 @@ print("balancedStringSplit")
 
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
-        num = 0
-        for v in range(len(s)):
-            print(v,s[v])
-        return num
+        return_num = 0
+        start = 0
+        for start in range(len(s)):
+            for i in range(1,round(len(s)/2)):
+                num = i+1
+                v = [s[j: j+num] for j in range(start, len(s), num)]
+                print(v)
+                # for v in range(len(s)):
+        return return_num
 
 s ="RLRRLLRLRL"
 solution = Solution()
