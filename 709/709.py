e2b1e588ac94752d932c8e9a14c531dc5f65dcee
zukaishi <ishizuka215@gmail.com>
2021-04-19 06:58:04 +0900

    update

---
+++ 709/709.py
@@ -0,0 +1,8 @@
+print("toLowerCase")
+
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return ""
+
+solution = Solution()
+print(solution.toLowerCase("ABC"))
---
+++ 709/README.md
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/to-lower-case/
+
+```python
+
+```
