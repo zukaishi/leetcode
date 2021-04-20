91c1852081f79f886ac5086a156f0c715020199a
zukaishi <ishizuka215@gmail.com>
2021-04-21 07:14:18 +0900

    update

--- 1812/1812.py
+++ 1812/1812.py
@@ -2,7 +2,10 @@ print("squareIsWhite")
 
 class Solution:
     def squareIsWhite(self, coordinates: str) -> bool:
-        return True
+        return_bool = False
+        if (ord(coordinates[0:1])-96)% 2 != int(coordinates[1:2]) % 2:
+            return_bool = True
+        return return_bool
 
 solution = Solution()
-print(solution.squareIsWhite("a1"))
+print(solution.squareIsWhite("b1"))
