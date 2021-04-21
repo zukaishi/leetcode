ad7bad2a5c5a6d74433f26f73dd821e8af76ca3a
zukaishi <ishizuka215@gmail.com>
2021-04-22 06:40:28 +0900

    update

---
+++ 1323/1323.py
@@ -0,0 +1,8 @@
+print("maximum69Number")
+
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        return 0
+
+solution = Solution()
+print(solution.maximum69Number(9669))
---
+++ 1323/README.md
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/maximum-69-number/
+
+```python
+
+```
