74ca81cff6ec3b9ae7bf7f2ed243a612ae34f5af
zukaishi <ishizuka215@gmail.com>
2021-04-25 06:17:49 +0900

    update

---
+++ 1304/1304.py
@@ -0,0 +1,10 @@
+from typing import List
+print("sumZero")
+
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        arr = [1,5,2,4,1]
+        return arr
+
+solution = Solution()
+print(solution.sumZero(5))
---
+++ 1304/README.md
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+
+```python 
+
+```
