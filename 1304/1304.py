0b4c76291b743e27af6972f1f3294e2d36dd7545
zukaishi <ishizuka215@gmail.com>
2021-04-25 06:36:50 +0900

    update

--- 1304/1304.py
+++ 1304/1304.py
@@ -3,8 +3,17 @@ print("sumZero")
 
 class Solution:
     def sumZero(self, n: int) -> List[int]:
-        arr = [1,5,2,4,1]
+        arr = []
+        if n % 2 != 0:
+            arr.append(0)
+        if n == 1:
+            return arr
+        max = 1000
+        for _ in range(0,int(n/2)):
+            arr.append(int(max))
+            arr.append(int(max*-1))
+            max -= 1
         return arr
 
 solution = Solution()
-print(solution.sumZero(5))
+print(solution.sumZero(10))
--- 1304/README.md
+++ 1304/README.md
@@ -1,5 +1,17 @@
 https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
 
 ```python 
-
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        arr = []
+        if n % 2 != 0:
+            arr.append(0)
+        if n == 1:
+            return arr
+        max = 1000
+        for _ in range(0,int(n/2)):
+            arr.append(int(max))
+            arr.append(int(max*-1))
+            max -= 1
+        return arr
 ```
