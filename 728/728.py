4b74982dbbf0c31869d485202496e4f3c303b86f
zukaishi <ishizuka215@gmail.com>
2021-04-27 06:15:56 +0900

    update

---
+++ 728/728.py
@@ -0,0 +1,10 @@
+from typing import List
+print("selfDividingNumbers")
+
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        r = []
+        return r
+
+solution = Solution()
+print(solution.selfDividingNumbers(1,22))
---
+++ 728/README.md
@@ -0,0 +1,6 @@
+https://leetcode.com/problems/self-dividing-numbers/
+
+```python
+
+
+```
