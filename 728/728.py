daaf775e97d7a130082b2d3fd2dc9f7d45656b77
zukaishi <ishizuka215@gmail.com>
2021-04-27 06:21:22 +0900

    update

--- 728/728.py
+++ 728/728.py
@@ -4,7 +4,13 @@ print("selfDividingNumbers")
 class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
         r = []
+        for i in range(left,right+1):
+            if i < 10:
+                r.append(i)
+            else:
+                str_i = str(i)
+                for j in range(0,len(str_i)):
+                    print(str_i[j])
         return r
-
 solution = Solution()
 print(solution.selfDividingNumbers(1,22))
