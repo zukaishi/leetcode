63314abc17204f8d45dd195d33b32faba45c57a3
zukaishi <ishizuka215@gmail.com>
2021-04-28 06:31:45 +0900

    update

--- 1837/1837.py
+++ 1837/1837.py
@@ -2,8 +2,17 @@ print("sumBase")
 
 class Solution:
     def sumBase(self, n: int, k: int) -> int:
+        arr = []
+        nn = n
         num = 0
+        for _ in range(0,n):
+            nn,kk = divmod(nn, k)
+            arr.append(kk)
+            if nn == 0:
+                break
+        for v in arr:
+            num += int(v)
         return num
 
 solution = Solution()
-print(solution.sumBase(34,6))
+print(solution.sumBase(42,2))
