2405407660c9b52e587628a8650320fa424e3058
zukaishi <ishizuka215@gmail.com>
2021-04-29 05:50:08 +0900

    update

--- 905/905.py
+++ 905/905.py
@@ -3,8 +3,14 @@ print("sortArrayByParity")
 
 class Solution:
     def sortArrayByParity(self, A: List[int]) -> List[int]:
-        r = []
-        return r
+        g = []
+        k = []
+        for v in A:
+            if v % 2 == 0:
+                g.append(v)
+            else:
+                k.append(v)
+        return g + k
 
 a =[3,1,2,4]
 solution = Solution()
