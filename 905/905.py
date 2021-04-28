e5ad9fcf73170611ad694c47048d083a56afa1e2
zukaishi <ishizuka215@gmail.com>
2021-04-29 05:46:08 +0900

    update

---
+++ 905/905.py
@@ -0,0 +1,11 @@
+from typing import List
+print("sortArrayByParity")
+
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        r = []
+        return r
+
+a =[3,1,2,4]
+solution = Solution()
+print(solution.sortArrayByParity(a))
