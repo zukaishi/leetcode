926f6c223afa59929b693799f51cd07bc92d9d5e
zukaishi <ishizuka215@gmail.com>
2021-04-30 06:32:10 +0900

    update

---
+++ 1748/1748.py
@@ -0,0 +1,12 @@
+
+from typing import List
+print("sumOfUnique")
+
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        num = 0
+        return num
+
+nums = [1,2,3,2]
+solution = Solution()
+print(solution.sumOfUnique(nums))
---
+++ 1748/README.md
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/sum-of-unique-elements/
+
+```python
+
+```
