533ef82eef6fdbcaaffc871b309bc1c178c42de5
zukaishi <ishizuka215@gmail.com>
2021-05-01 05:42:53 +0900

    update

---
+++ 657/657.py
@@ -0,0 +1,9 @@
+print("judgeCircle")
+
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        r = true
+        return r
+
+solution = Solution()
+print(solution.judgeCircle("UD"))
---
+++ 657/README.md
@@ -0,0 +1,6 @@
+https://leetcode.com/problems/robot-return-to-origin/
+
+```python
+
+
+```
