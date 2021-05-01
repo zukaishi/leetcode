a348903e28461b093671fe85d6aa685045dc0e35
zukaishi <ishizuka215@gmail.com>
2021-05-02 06:42:37 +0900

    update

---
+++ 942/942.py
@@ -0,0 +1,10 @@
+from typing import List
+print("diStringMatch")
+
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        r = []
+        return r
+
+solution = Solution()
+print(solution.diStringMatch("IDID"))
---
+++ 942/README.md
@@ -0,0 +1,6 @@
+https://leetcode.com/problems/di-string-match/
+
+```python
+
+
+```
