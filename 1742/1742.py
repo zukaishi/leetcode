82b0659517b4d6825c8d6e2ad3bbb974106a26bc
zukaishi <ishizuka215@gmail.com>
2021-05-03 06:37:55 +0900

    update

--- 1742/1742.py
+++ 1742/1742.py
@@ -3,7 +3,20 @@ print("countBalls")
 class Solution:
     def countBalls(self, lowLimit: int, highLimit: int) -> int:
         r = 0
-        return r
+        d = {}
+        for i in range(lowLimit,highLimit+1):
+            a = 0
+            si = str(i)
+            if len(si) == 1:
+                a = i
+            else:
+                for j in range(0,len(si)):
+                    a += int(si[j])
+            if a in d:
+                d[a] += 1
+            else:
+                d[a] = 1
+        return max(d.values())
 
 solution = Solution()
-print(solution.countBalls(1, 10))
+print(solution.countBalls(5, 15))
