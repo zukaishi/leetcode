6853df85f9d07e7f086d905fe5c73afd389f4349
zukaishi <ishizuka215@gmail.com>
2021-05-04 05:55:31 +0900

    update

---
+++ 461/461.py
@@ -0,0 +1,9 @@
+print("hammingDistance")
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        r = 0
+        return r
+
+solution = Solution()
+print(solution.hammingDistance(1, 4))
