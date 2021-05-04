b3cd2f70b730884751c4884d80c49bc5d320a404
zukaishi <ishizuka215@gmail.com>
2021-05-05 06:32:13 +0900

    update

--- 557/557.py
+++ 557/557.py
@@ -3,7 +3,11 @@ print("reverseWords")
 class Solution:
     def reverseWords(self, s: str) -> str:
         r = ""
-        return r
+        for v in s.split():
+            for i in range(len(v)-1,-1,-1):
+                r += v[i]
+            r += " "
+        return r.strip(" ")
 
 solution = Solution()
 print( solution.reverseWords("Let's take LeetCode contest"))
--- 557/README.md
+++ 557/README.md
@@ -1,5 +1,12 @@
 https://leetcode.com/problems/reverse-words-in-a-string-iii/
 
 ```python
-
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        r = ""
+        for v in s.split():
+            for i in range(len(v)-1,-1,-1):
+                r += v[i]
+            r += " "
+        return r.strip(" ")
 ```
