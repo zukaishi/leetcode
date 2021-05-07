64bca7c855a39f868549008df98c5ecc2f9af777
zukaishi <ishizuka215@gmail.com>
2021-05-08 06:29:03 +0900

    update

--- 1051/1051.py
+++ 1051/1051.py
@@ -4,8 +4,12 @@ print("heightChecker")
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         r = 0
+        heights_s =  sorted(heights)
+        for i in range(0,len(heights)):
+            if heights[i] != heights_s[i]:
+                r += 1
         return r
 
-heights = [1,1,4,2,1,3]
+heights = [5,1,2,3,4]
 solution = Solution()
 print(solution.heightChecker(heights))
--- 1051/README.md
+++ 1051/README.md
@@ -1,5 +1,12 @@
 https://leetcode.com/problems/height-checker/
 
 ```python
-
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        r = 0
+        heights_s =  sorted(heights)
+        for i in range(0,len(heights)):
+            if heights[i] != heights_s[i]:
+                r += 1
+        return r
 ```
