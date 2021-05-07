e131c398ba3edceb9a2d93942e48d7ccc9f70da4
zukaishi <ishizuka215@gmail.com>
2021-05-08 06:22:09 +0900

    update

---
+++ 1051/1051.py
@@ -0,0 +1,11 @@
+from typing import List
+print("heightChecker")
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        r = 0
+        return r
+
+heights = [1,1,4,2,1,3]
+solution = Solution()
+print(solution.heightChecker(heights))
---
+++ 1051/README.md
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/height-checker/
+
+```python
+
+```
