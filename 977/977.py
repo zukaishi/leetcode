571a1862cd26afb28bd4248e6a6c1396b89ab65c
zukaishi <ishizuka215@gmail.com>
2021-05-10 06:09:02 +0900

    update

--- 977/977.py
+++ 977/977.py
@@ -3,7 +3,10 @@ print("sortedSquares")
 
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        return []
+        r = []
+        for v in nums:
+            r.append(v**2)
+        return sorted(r)
 
 nums = [-4,-1,0,3,10]
 solution = Solution()
--- 977/README.md
+++ 977/README.md
@@ -1,5 +1,10 @@
 https://leetcode.com/problems/squares-of-a-sorted-array/
 
 ```python
-
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        r = []
+        for v in nums:
+            r.append(v**2)
+        return sorted(r)
 ```
