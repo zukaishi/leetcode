1d612e7a606f3e793518e2f01131f06f7080247e
zukaishi <ishizuka215@gmail.com>
2021-05-10 06:06:12 +0900

    update

---
+++ 977/977.py
@@ -0,0 +1,10 @@
+from typing import List
+print("sortedSquares")
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        return []
+
+nums = [-4,-1,0,3,10]
+solution = Solution()
+print(solution.sortedSquares(nums))
